aeacec1a96e9a20ccab9f056d3fcdbee8f12e263
howie50417 <howie50417@gmail.com>
2024-01-29 12:36:13 +0800

    test987

---
+++ test987/hybrid/html/js/config.js
@@ -0,0 +1,14 @@
+var playerConfig = {
+    container:'#mui-player',
+    src:'http://vfx.mtime.cn/Video/2021/07/09/mp4/210709172715355157.mp4',
+    // poster:'https://muiplayer.oss-cn-shanghai.aliyuncs.com/static/image/poster.jpg',
+    videoAttribute:[
+        {attrKey:'webkit-playsinline',attrValue:''},
+        {attrKey:'playsinline',attrValue:''},
+        {attrKey:'x5-playsinline',attrValue:''},
+        {attrKey:'t7-video-player-type',attrValue:'inline'},
+        {attrKey:'x5-video-player-type',attrValue:'h5-page'},
+        {attrKey:'x-webkit-airplay',attrValue:'allow'},
+        {attrKey:'controlslist',attrValue:'nodownload'},
+    ],
+}
